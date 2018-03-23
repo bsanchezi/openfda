1b7cf7926f73cba56c4589b76dbbec03d60a5b47
bsanchezi <19beatriz5c@gmail.com>
2018-03-23 18:46:27 +0100

    Práctica 1 programa 1 prueba

---
+++ openfda-1/programa1.py
@@ -0,0 +1,22 @@
+import http.client
+import json
+
+headers = {'User-Agent': 'http-client'}
+
+conn = http.client.HTTPSConnection("api.fda.gov")   #pagina para acceder a la API REST de openFDA
+conn.request("GET", "/drug/label.json", None, headers)   #recurso al que se quiere acceder
+r1 = conn.getresponse() #respuesta de la info
+print(r1.status, r1.reason)  #200 OK   #comprobar que funciona
+informacion_label = r1.read().decode("utf-8")  #guardamos en una variable toda la información consultada
+conn.close()
+
+label_mejorada = json.loads(informacion_label)  #mejoramos la info obtenida
+medicamento=label_mejorada['results']   #nos quedamos solo con la info del apartado results, quitando meta
+
+print('ID: ', medicamento['id'])   #seleccionamos la informacion requerida fijándonos en la indexacion
+                                    #que tiene en el archivo json
+print('Proposito: ', medicamento['purpose'])
+print('Fabricante(s): ', medicamento['manufacturer_name'])
+
+
+
